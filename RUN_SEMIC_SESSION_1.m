aea6f8334d0a1163db7360f304cf68abe4b37790
Suhwan Gim <roseno.9@daum.net>
2018-12-14 16:21:07 +0900

    re-organizing folder

--- RUN_SEMIC_SESSION_1.m
+++ RUN_SEMIC_SESSION_1.m
@@ -1,21 +1,24 @@
-%% DAY1
+%% SESSION 1
 clc;
 clear;
 close all;
+
+%% SETUP: DIRECTORY
+% ~/functions
+% ~/task_functions
+addpath(genpath(pwd));
 %% SETUP: PARAMETER
+% 1) PC A: computer connected with the Pathway and also inter-connected
+% comput B 
+% 2) PC B: computer for present stimulus and send command to trigger
+% pathway also connected with PC A using TCP-IP protocal(i.e., hub or cross
+% UTP calbe)
+
+% should put the PC A's IP and port
 ip = '192.168.0.3'; %ip = '115.145.189.133'; %ip = '203.252.54.21';
 port = 20121;
-addpath(genpath(pwd));
-
-
-
 %% 1. Calibration
-SID = 입력하세요;
-calibration(SID, ip, port,'joystick','test');
+SID = ''; %subject ID 
+calibration(SID, ip, port,'joystick','test'); % run calibration task 
 
-
-
-
-
-%% 
-    SEMIC_Senddata(SID,'Calibration')
+%% ===================================================================== %% 
--- RUN_SEMIC_SESSION_2_MRI.m
+++ RUN_SEMIC_SESSION_2_MRI.m
@@ -1,22 +1,33 @@
-%% DAY2
+%% SESSION 2 
 clc;
 clear;
 close all;
+%% SETUP: DIRECTORY
+% ~/functions
+% ~/task_functions
+addpath(genpath(pwd));
 %% SETUP: PARAMETER
-%ip = '115.145.189.133'; 
-ip = '192.168.0.3'; port = 20121; addpath(genpath(pwd));
+% 1) PC A: computer connected with the Pathway and also inter-connected
+% comput B 
+% 2) PC B: computer for present stimulus and send command to trigger
+% pathway also connected with PC A using TCP-IP protocal(i.e., hub or cross
+% UTP calbe)
+
+% should put the PC A's IP and port
+ip = '192.168.0.3'; %ip = '115.145.189.133'; %ip = '203.252.54.21';
+port = 20121;
 %% SETUP: Load calibration data & SkinSite sequences
 reg = load_cali_results(); disp(reg.skinSite_rs);
 
 %% EXPERIMENT %% 
-%% 1. Rest run
-    SID = 입력하세요 %Participants
+%% 1. Resting-state run
+    SID = ''; %subject ID
     rest_run(SID,'fmri','biopac1','eyelink','test');
-%% 2. T1 & Learning phase
+%% 2. Structual imaging & Learning phase
     Learning_test(SID, ip, port, reg,'fmri','biopac1','joystick','eyelink','test');
-%% 3. Motor TASK
+%% 3. Simple motor task 1
     Motor_practice(SID, 1,'fmri','biopac1','joystick','eyelink','test');
-%% Main STUDY % thermode_test(('biopac1','fmri') ... ... ),
+%% Main task: using fucntion thermode_test(('biopac1','fmri') ... ... )
 %% 4. RUN1 
     thermode_test(SID, 1, ip, port, reg,'fmri','biopac1','joystick','eyelink','test');
 %% 5. RUN2
@@ -25,7 +36,7 @@ reg = load_cali_results(); disp(reg.skinSite_rs);
     thermode_test(SID, 3, ip, port, reg,'fmri','biopac1','joystick','eyelink','test');
     
     
-%% 7. Motor task
+%% 7. Simple motor task 2
     Motor_practice(SID, 2,'fmri','biopac1','joystick','eyelink','test');
 %% 8. RUN4
     thermode_test(SID, 4, ip, port, reg,'fmri','biopac1','joystick','eyelink','test');
@@ -34,7 +45,3 @@ reg = load_cali_results(); disp(reg.skinSite_rs);
 %% 10. RUN6
     thermode_test(SID, 6, ip, port, reg,'fmri','biopac1','joystick','eyelink','test');
    
-    
-    
-%% SEND DATA
-    SEMIC_Senddata(SID,'mri')
